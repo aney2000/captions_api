5127b6b6fb08c2e0f5f2aebd43d665517bc8b89a
Dragan Andrei <aney2000@yahoo.com>
2026-07-24 21:11:10 +0300

    modify default controller to show the results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  def render_result(result, success_status: nil)
+    if result.success?
+      render json: result.payload, status: (success_status || result.status)
+    else
+      render json: result.error, status: result.status
+    end
+  end
+
+  def with_authenticated_user
+    result = AuthenticateRequest.new.call(request.headers["Authorization"])
+    return render json: result.error, status: result.status if result.failure?
+
+    yield result.payload
+  end
 end
